cfb8dcd91aecc4dc98063d81f93debc44391efed
guilhermo.malater <guilhermoh@gmail.com>
2026-09-06 12:13:07 -0300

    feat: add sellers entity

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.address import Address
+from app.models.seller import Seller
 from app.models.user import User
 
-__all__ = ["Address", "User"]
+__all__ = ["Address", "Seller", "User"]
